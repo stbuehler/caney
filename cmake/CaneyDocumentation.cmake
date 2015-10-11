a550727e8bc36316d9ee5b2fd63351cce75bc0eb
Stefan Bühler <stbuehler@web.de>
2015-10-11 19:16:59 +0200

    [cmake] fix include paths for doxygen

--- cmake/CaneyDocumentation.cmake
+++ cmake/CaneyDocumentation.cmake
@@ -3,6 +3,12 @@ set(CANEY_CREATE_DOCUMENTATION "ON" CACHE BOOL "whether to build documentation")
 if(CANEY_CREATE_DOCUMENTATION)
 	find_package(Doxygen REQUIRED)
 
+	file(GLOB components_includes components/*/include)
+	set(INCLUDE_PATH)
+	foreach(incl ${components_includes})
+		set(INCLUDE_PATH "${INCLUDE_PATH} \"${incl}\"")
+	endforeach()
+
 	configure_file("${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in" "${CMAKE_CURRENT_BINARY_DIR}/Doxyfile" @ONLY)
 
 	add_custom_target(doc
